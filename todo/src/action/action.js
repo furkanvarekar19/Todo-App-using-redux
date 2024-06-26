4776b891d6bacdfbb3616e9049d50cbb1d463110
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-06-26 11:26:45 +0530

    update

---
+++ todo/src/action/action.js
@@ -0,0 +1,14 @@
+// action types
+export const ADDTODO = "ADDTODO";
+export const REMOVETODO = "REMOVETODO";
+
+// action creators
+const addtodo = () =>{
+    return {type: ADDTODO};
+};
+const removetodo = () =>{
+    return {type: REMOVETODO};
+};
+
+
+
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+// import { ADDTODO,REMOVETODO } from "../action/action";
+
+const initialState = {
+    todos: []
+  };
+
+const todoReducer = (state = initialState , action) =>{
+   switch(action.type){
+ 
+   }
+}
+
+export default todoReducer;
